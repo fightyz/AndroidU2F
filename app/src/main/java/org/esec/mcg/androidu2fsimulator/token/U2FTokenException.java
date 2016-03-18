e0a8613050943e9dd0a3f6b3f202e3324dbc8757
Yang Zhou <yangzhou@std.uestc.edu.cn>
2016-03-18 16:22:00 +0800

    Strip the simulator token

--- app/src/main/java/org/esec/mcg/androidu2fsimulator/token/U2FTokenException.java
+++
@@ -1,9 +0,0 @@
-package org.esec.mcg.androidu2fsimulator.token;
-
-/**
- * Created by yz on 2016/3/16.
- */
-public class U2FTokenException extends Exception {
-    public U2FTokenException(String message) { super(message); }
-    public U2FTokenException(String message, Throwable cause) { super(message, cause); }
-}
--- app/src/main/java/org/esec/mcg/androidu2fsimulator/token/msg/U2FTokenIntentType.java
+++
@@ -1,9 +0,0 @@
-package org.esec.mcg.androidu2fsimulator.token.msg;
-
-/**
- * Created by yz on 2016/3/16.
- */
-public enum U2FTokenIntentType {
-    U2F_OPERATION_REG,
-    U2F_OPERATION_SIGN
-}
